23bc32d567ef763b85c675ba2f34b1fa2e50f141
Lalith2001 <55174072+Lalith2001@users.noreply.github.com>
2022-01-20 14:09:20 +0530

    Add files via upload

---
+++ javaday1/src/javaday1/FirstJava.java
@@ -0,0 +1,11 @@
+package javaday1;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello this is my placement java classes");
+
+	}
+
+}
