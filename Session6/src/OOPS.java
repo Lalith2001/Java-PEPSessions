4623791264b3c081c7de81b80f5d1aeab0e03df7
Gundu Lalithendranath <55174072+Lalith2001@users.noreply.github.com>
2022-01-25 23:57:42 +0530

    Add files via upload

---
+++ Session6/src/OOPS.java
@@ -0,0 +1,37 @@
+
+class User{ // this is the definition of User Object
+    
+    // Attributes: Property of Object and not of class i.e. they belong to object and not to class
+    String name;
+    String phone;
+    String email;
+    char gender;
+    String addressLine;
+    String city;
+    String state; 
+    int zipCode;
+    
+    User()
+    {
+    	name="Lalith";
+    	phone="+910000 00000";
+    	email="example@example.com";
+    	gender='N';
+    	addressLine="Halloween street";
+    	city="Hyderabad";
+    	state="Telangana";
+    	zipCode=17001;
+    	
+    }
+    void show() {
+    	
+    }
+}
+
+public class OOPS {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
